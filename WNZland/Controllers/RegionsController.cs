4b1c45ad94f21b3961109bc2cbd08bb8122346f6
Mohammed Ali <102896991+mohammedAliFish@users.noreply.github.com>
2025-02-02 20:28:52 +0300

    complete get and post

--- WNZland/Controllers/RegionsController.cs
+++ WNZland/Controllers/RegionsController.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.AspNetCore.Mvc;
 using WNZland.Data;
+using WNZland.Models.Domain;
+using WNZland.Models.DTO;
 
 namespace WNZland.Controllers
 {
@@ -14,22 +16,67 @@ namespace WNZland.Controllers
             this.dbContext = dbContext;
         }
 
-         [HttpGet]
-         public IActionResult GetAll()
-         {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
             var regions = dbContext.Regions.ToList();
-            return Ok(regions);
-         }
 
-            [HttpGet("{id:Guid}")]
+            var regionDto = new List<RegionDto>();
 
-            public IActionResult GetById([FromRoute] Guid id)
+            foreach (var region in regions)
             {
-               // var region = dbContext.Regions.Find(id);
-               var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
-                if(region == null)
-                return NotFound();
-                return Ok(region);
+                regionDto.Add(new RegionDto
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                });
             }
+
+            return Ok(regionDto);
+        }
+
+        [HttpGet("{id:Guid}")]
+
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // var region = dbContext.Regions.Find(id); or
+            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+            if (region == null)
+                return NotFound();
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            
+          
+            return Ok(regionDto);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto regionDto)
+        {
+            var regionDomain = new Region
+            {
+                Name = regionDto.Name,
+                Code = regionDto.Code,
+                RegionImageUrl = regionDto.RegionImageUrl
+            };
+            dbContext.Regions.Add(regionDomain);
+            dbContext.SaveChanges();
+
+            var regionDtos = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDtos.Id }, regionDtos);
+    
+        }
     }
 }
---
+++ WNZland/Models/DTO/AddRegionRequestDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WNZland.Models.DTO;
+
+public class AddRegionRequestDto
+{
+public string Code { get; set; }
+
+public string Name { get; set; }
+public string? RegionImageUrl { get; set; }
+}
