3107307c967161a32da9fb47780f9aa167b9e215
Mohammed Ali <102896991+mohammedAliFish@users.noreply.github.com>
2025-02-11 22:48:29 +0300

    complete seeding

--- WNZland/obj/Debug/net8.0/WNZland.AssemblyInfo.cs
+++ WNZland/obj/Debug/net8.0/WNZland.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WNZland")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fc6dfb51fcfe3f4899da02359dd43fb358247b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c6150cb1f529eb5696b5935b0744f5a7f90bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WNZland")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WNZland")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
